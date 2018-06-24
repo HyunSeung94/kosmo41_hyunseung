558a18a09fbe55b084eaf53b6c5dfd3894b53273
김현승 <rhqorwnd08@gmail.com>
2018-06-24 12:27:03 +0900

    6/24 과제 완료

---
+++ java/ch05/src/Quiz13.java
@@ -0,0 +1,25 @@
+
+public class Quiz13 {
+
+	public static void main(String[] args) {
+
+		System.out.println("가로");
+		for(int i = 1; i<10; i++) {
+			for(int j = 2; j<10; j++) {
+				System.out.print(j + " * " +i + "= " + (j*i)+"\t");
+			}
+			System.out.print('\n');
+
+		}
+		System.out.println("세로");
+		for(int i = 2; i<10; i++) {
+			for(int j = 1; j<10; j++) {
+
+				System.out.print(i + " * " +j + "= " + (i*j)+"  ");
+			}
+			System.out.print('\n');
+		}
+
+		
+	}
+	}
--- java/ch05/src/quiz12.java
+++ java/ch05/src/quiz12.java
@@ -1,7 +1,20 @@
-
+/*문제 05-12
+Do~While문을 이용하여 1~1000까지 더해서 결과를 출력하는 프로그램을 작성하시오.
+실행결과도 아래와같이 출력하시오.
+예)1+2+3.......+1000 = 500500
+*/
 public class quiz12 {
 
 	public static void main(String[] args) {
+		int sum = 0;
+		int num = 1;
+		do {
+			System.out.print(num+ " + " );
+			sum = sum + num;
+			num++;
+		}while(num <= 1000);
+		System.out.println("=" +sum);
+		
 		
 
 	}
