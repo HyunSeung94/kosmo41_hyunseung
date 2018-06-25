4447cfc3e1206fff5709b36e7253d124b013d9e3
hyunseung(2) <gustmd8593@naver.com>
2018-06-25 18:35:08 +0900

    6/25학원수업수정

--- java/ch05/src/Quiz12.java
+++ java/ch05/src/Quiz12.java
@@ -3,7 +3,7 @@ Do~While문을 이용하여 1~1000까지 더해서 결과를 출력하는 프로
 실행결과도 아래와같이 출력하시오.
 예)1+2+3.......+1000 = 500500
 */
-public class quiz12 {
+public class Quiz12 {
 
 	public static void main(String[] args) {
 		int sum = 0;
