466ac08e25f70ac6c873a676937238c4b60f00c9
김현승 <rhqorwnd08@gmail.com>
2018-07-05 21:55:31 +0900

    과제 수정

--- java/game/src/TicTacToe.java
+++ java/game/src/TicTacToe.java
@@ -6,12 +6,14 @@ public class TicTacToe {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		String arr[][] = { { "1", "2", "3" }, { "4", "5", "6" }, { "7", "8", "9" } };
+		String arr1[][] = { { "1", "2", "3" }, { "4", "5", "6" }, { "7", "8", "9" } };
 		int a = 2;
 		int b;
 		int c;
 		String change = "X";
 		String change1 = "O";
 		do {
+			
 			for (int i = 0; i < arr.length; i++) {
 				for (int j = 0; j < arr.length; j++) {
 					System.out.print(arr[i][j]);
@@ -24,7 +26,7 @@ public class TicTacToe {
 					System.out.print("--|---|---");
 					System.out.println();
 				}
-
+				
 			}
 			System.out.println();
 			if(change == arr[0][0] && change == arr[0][1] && change == arr[0][2]||
@@ -32,17 +34,23 @@ public class TicTacToe {
 			   change == arr[2][0] && change == arr[2][1] && change == arr[2][2]||	   
 			   change == arr[0][0] && change == arr[1][0] && change == arr[2][0]||
 			   change == arr[0][1] && change == arr[1][1] && change == arr[2][1]||
-			   change == arr[0][2] && change == arr[1][2] && change == arr[2][2])
+			   change == arr[0][2] && change == arr[1][2] && change == arr[2][2]||
+			   change == arr[0][0] && change == arr[1][1] && change == arr[2][2]||
+			   change == arr[2][0] && change == arr[1][1] && change == arr[0][2])
 			{
 				System.out.println("Player 1 has wonn");
 				break;
 			}
+			
 			if(change1 == arr[0][0] && change1 == arr[0][1] && change1 == arr[0][2]||
 			   change1 == arr[1][0] && change1 == arr[1][1] && change1 == arr[1][2]||
 			   change1 == arr[2][0] && change1 == arr[2][1] && change1 == arr[2][2]||
 			   change1 == arr[0][0] && change1 == arr[1][0] && change1 == arr[2][0]||
 			   change1 == arr[0][1] && change1 == arr[1][1] && change1 == arr[2][1]||
-			   change1 == arr[0][2] && change1 == arr[1][2] && change1 == arr[2][2]) {
+			   change1 == arr[0][2] && change1 == arr[1][2] && change1 == arr[2][2]||
+			   change1 == arr[0][0] && change1 == arr[1][1] && change1 == arr[2][2]||
+			   change1 == arr[2][0] && change1 == arr[1][1] && change1 == arr[0][2])
+			{
 						System.out.println("Player 2 has wonn");
 						break;
 					}
@@ -112,7 +120,11 @@ public class TicTacToe {
 
 			}
 			a++;
-			
+			if(a==11) {
+				System.out.println("허용 한도가 초과되었습니다. 다시시작!!");
+				arr = arr1; 
+			}
+
 		
 
 		} while (true);
--- java/game/src/test.java
+++ java/game/src/test.java
@@ -1,193 +1,125 @@
 import java.util.Arrays;
-import java.util.Random;
 import java.util.Scanner;
 
 public class test {
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		Random rand = new Random();
-		String arr[][] = { { "1", "2", "3" }, { "4", "5", "6" }, { "7", "x", "8" } };
-		String arr1[][] = { { "1", "2", "3" }, { "4", "5", "6" }, { "7", "8", "x" } };
-		String change;
-		String a;
-
-		int c = 0;
-		do {
-			int b = rand.nextInt(4);
-			if (b == 0) {
-				for (int i = 0; i < arr.length; i++) {
-					for (int j = 0; j < arr.length; j++) {
-
-						if (arr[i][j] == "x") {
-							if (j <= 0) {
-								continue;
-							}
-							change = arr[i][j];
-							arr[i][j] = arr[i][j - 1];
-							arr[i][j - 1] = change;
-						}
-					}
-				}
-				System.out.println();
-			}
-
-			if (b == 1) {
-
-				for (int i = 0; i < arr.length; i++) {
-					for (int j = 0; j < arr.length; j++) {
-						if (arr[i][j] == "x") {
-							if (j >= 2) {
-								continue;
-							}
-							change = arr[i][j];
-							arr[i][j] = arr[i][j + 1];
-							arr[i][j + 1] = change;
-
-						}
-					}
-				}
-				System.out.println();
-			}
-			if (b == 2) {
-				for (int i = 0; i < arr.length; i++) {
-					for (int j = 0; j < arr.length; j++) {
-						if (arr[i][j] == "x") {
-							if (i >= 2) {
-								continue;
-							}
-							i++;
-							change = arr[i][j];
-							arr[i][j] = arr[i - 1][j];
-							arr[i - 1][j] = change;
-
-						}
-					}
-				}
-				System.out.println();
-			}
-
-			if (b == 3) {
-				for (int i = 0; i < arr.length; i++) {
-					for (int j = 0; j < arr.length; j++) {
-						if (arr[i][j] == "x") {
-							if (i <= 0) {
-
-								continue;
-							}
-							i--;
-							change = arr[i][j];
-							arr[i][j] = arr[i + 1][j];
-							arr[i + 1][j] = change;
-
-						}
-					}
-				}
-				System.out.println();
-			}
-			c++;
-
-		} while (c < 100);
-
-		for (;;) {
-
+		String arr[][] = { { "1", "2", "3" }, { "4", "5", "6" }, { "7", "8", "9" } };
+		String arr1[][] = { { "1", "2", "3" }, { "4", "5", "6" }, { "7", "8", "9" } };
+		int a = 2;
+		int b;
+		int c=0;
+		String change = "X";
+		String change1 = "O";
+		do {	
 			for (int i = 0; i < arr.length; i++) {
 				for (int j = 0; j < arr.length; j++) {
 					System.out.print(arr[i][j]);
-					if(j<2) {
+					if (j < 2) {
 						System.out.print(" | ");
 					}
 				}
-				if(i<2) {
-				System.out.println();
-				System.out.print("--|---|--");
-				System.out.println();
+				if (i < 2) {
+					System.out.println();
+					System.out.print("--|---|---");
+					System.out.println();
+				}	
+			}
+			System.out.println();
+			if ((a % 2) == 0) {
+				System.out.println("Player1, Please enter the number of the square ");
+				System.out.println("where you want to place your X:");
+				b = s.nextInt();
+				if (b == 1) {
+					arr[0][0] = change;
 				}
-			}System.out.println();
-
-			System.out.println("[ Move ] a:Left s:Right w:Up z:Down");
-			System.out.println("[ Exit ] k:Exit");
-			System.out.println("이동키를 입력하세요");
-			a = s.next();
-
-			if (a.equals("a")) {
-				for (int i = 0; i < arr.length; i++) {
-					for (int j = 0; j < arr.length; j++) {
-
-						if (arr[i][j] == "x") {
-							if (j <= 0) {
-								System.out.println("잘못입력했어요");
-								continue;
-							}
-							change = arr[i][j];
-							arr[i][j] = arr[i][j - 1];
-							arr[i][j - 1] = change;
-						}
+				if (b == 2) {
+					arr[0][1] = change;
+				}
+				if (b == 3) {
+					arr[0][2] = change;
+				}
+				if (b == 4) {
+					arr[1][0] = change;
+				}
+				if (b == 5) {
+					arr[1][1] = change;
+				}
+				if (b == 6) {
+					arr[1][2] = change;
+				}
+				if (b == 7) {
+					arr[2][0] = change;
+				}
+				if (b == 8) {
+					arr[2][1] = change;
+				}
+				if (b == 9) {
+					arr[2][2] = change;
+				}
+			}
+			if ((a % 2) != 0) {
+				System.out.println("Player2, Please enter the number of the square ");
+				System.out.println("where you want to place your O:");
+				b = s.nextInt();
+				if (b == 1) {
+					arr[0][0] = change1;
+				}
+				if (b == 2) {
+					arr[0][1] = change1;
+				}
+				if (b == 3) {
+					arr[0][2] = change1;
+				}
+				if (b == 4) {
+					arr[1][0] = change1;
+				}
+				if (b == 5) {
+					arr[1][1] = change1;
+				}
+				if (b == 6) {
+					arr[1][2] = change1;
+				}
+				if (b == 7) {
+					arr[2][0] = change1;
+				}
+				if (b == 8) {
+					arr[2][1] = change1;
+				}
+				if (b == 9) {
+					arr[2][2] = change1;
+				}
+			}	
+			for(int i=0; i<arr.length; i++) {
+				for(int j=0; j<arr.length; j++) {
+					if(arr[i][0] == "X" && arr[i][1] =="X" && arr[i][2] =="X") {
+						c++;
+					}
+					if(arr[0][j] == "X" && arr[1][j] =="X" && arr[2][j] =="X") {
+						c++;
+					}
+					if(arr[i][0] == "O" && arr[i][1] =="O" && arr[i][2] =="O") {
+						c++; c++;
+					}
+					if(arr[0][j] == "O" && arr[1][j] =="O" && arr[2][j] =="O") {
+						c++; c++;
 					}
 				}
-				System.out.println();
+			}	
+			a++;
+			if(a==11) {
+				System.out.println("허용 한도가 초과되었습니다. 다시시작!!");
+				arr = arr1; 
 			}
-			if (a.equals("s")) {
-				for (int i = 0; i < arr.length; i++) {
-					for (int j = 0; j < arr.length; j++) {
-						if (arr[i][j] == "x") {
-							if (j >= 2) {
-								System.out.println("잘못입력했어요!! 다시입력하세요~");
-								continue;
-							}
-							change = arr[i][j];
-							arr[i][j] = arr[i][j + 1];
-							arr[i][j + 1] = change;
-							break;
-						}
-					}
-				}
-				System.out.println();
+			if(c==3) {
+				System.out.println("Plater 1 has wonn");break;
 			}
-			if (a.equals("z")) {
-				for (int i = 0; i < arr.length; i++) {
-					for (int j = 0; j < arr.length; j++) {
-						if (arr[i][j] == "x") {
-							if (i >= 2) {
-								System.out.println("잘못입력했어요!! 다시입력하세요~");
-								continue;
-							}
-							i++;
-							change = arr[i][j];
-							arr[i][j] = arr[i - 1][j];
-							arr[i - 1][j] = change;
-
-						}
-					}
-				}
-				System.out.println();
+			if(c==6) {
+				System.out.println("Player 2 has wonn");break;	
 			}
-			if (a.equals("w")) {
-				for (int i = 0; i < arr.length; i++) {
-					for (int j = 0; j < arr.length; j++) {
-						if (arr[i][j] == "x") {
-							if (i <= 0) {
-								System.out.println("잘못입력했어요!! 다시입력하세요~");
-								continue;
-							}
-							i--;
-							change = arr[i][j];
-							arr[i][j] = arr[i + 1][j];
-							arr[i + 1][j] = change;
-
-						}
-					}
-				}
-				System.out.println();
-			}
-			if(arr[0][0] == arr[0][1] == arr[0][2] )
-
-			if (Arrays.deepEquals(arr, arr1)) { // 배열끼리 비교문
-				System.out.println("정답입니다.. 수고하셧습니다! ");
-				break;
-			}
-
-		}
+		} while (true);
 
 	}
+
 }
