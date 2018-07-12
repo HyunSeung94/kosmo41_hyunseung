8ceb0c6924484a480d9d39a32b9e0be0a56916da
hyunseung(2) <gustmd8593@naver.com>
2018-07-12 19:30:05 +0900

    7월 12 수업끝

--- java/JavaChatB04/src/MultiClient2.java
+++ java/JavaChatB04/src/MultiClient2.java
@@ -1,5 +1,3 @@
-/* 콘솔 멀티채팅 클라이언트 프로그램 */
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,7 +6,7 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Scanner;
 
-public class MultClient {
+public class MultiClient2 {
 
 	public static void main(String[] args) throws UnknownHostException, IOException {
 		System.out.println("이름을 입력해 주세요.");
@@ -19,9 +17,9 @@ public class MultClient {
 		BufferedReader in = null;
 		
 		try {
-			//String ServerIP = "Localhost";
-			Socket socket = new Socket(args[0], 9999);  //소켓 객체 생성
-			System.out.println("서버와 시작되었습니다.");
+			String ServerIP = "192.168.0.121";
+			Socket socket = new Socket(ServerIP, 9999);  //소켓 객체 생성
+			System.out.println("서버와 연결이 되었습니다......");
 			
 			out = new PrintWriter(socket.getOutputStream(), true);
 			in = new BufferedReader(new InputStreamReader(
@@ -29,14 +27,32 @@ public class MultClient {
 			
 			out.println(s_name);
 			
-			System.out.println("Receive : " + in.readLine());
-			
+			while (out!=null) {
+				try {
+					//입력
+					if(in!= null) {
+						System.out.println("Receive : " + in.readLine());
+					}
+					
+					//출력
+					String s2 = s.nextLine();
+					if( s2.equals("q")|| s2.equals("Q")) {
+						out.println(s2);
+						break;
+					}else {
+						out.println(s_name+"=>"+s2);
+					}
+					}catch(IOException e) {
+						System.out.println("예외:" +e);
+				}
+			}
 			in.close();
 			out.close();
+			
 			socket.close();
 		}catch(Exception e) {
-			System.out.println("예외[MultiClient class]:"+e);
+			System.out.println("예외[MultiClient class"+e);
 		}
-	}
+}
 
 }
