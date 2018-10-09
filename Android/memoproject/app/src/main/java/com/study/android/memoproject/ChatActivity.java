5a0737a4658d3f649e6c67e597aeb89a486c1af8
김현승 <rhqorwnd08@gmail.com>
2018-10-09 22:56:39 +0900

    일요일 술먹기전 커밋

--- Android/memoproject/app/src/main/java/com/study/android/memoproject/ChatActivity.java
+++ Android/memoproject/app/src/main/java/com/study/android/memoproject/ChatActivity.java
@@ -1,20 +1,28 @@
 package com.study.android.memoproject;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class ChatActivity extends AppCompatActivity {
     // 채팅방
@@ -28,12 +36,18 @@ public class ChatActivity extends AppCompatActivity {
     private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
     private DatabaseReference databaseReference = firebaseDatabase.getReference();
 
+    DataSnapshot dataSnapshot;
+
+    // 이메일
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
-// 위젯 ID 참조
+        mAuth = FirebaseAuth.getInstance();
+        // 위젯 ID 참조
         chat_view = (ListView) findViewById(R.id.chat_view);
         chat_edit = (EditText) findViewById(R.id.chat_edit);
         chat_send = (Button) findViewById(R.id.chat_sent);
@@ -53,19 +67,23 @@ public class ChatActivity extends AppCompatActivity {
                 if (chat_edit.getText().toString().equals(""))
                     return;
 
-                ChatDTO chat = new ChatDTO(USER_NAME, chat_edit.getText().toString()); //ChatDTO를 이용하여 데이터를 묶는다.
-                databaseReference.child("chat").child(CHAT_NAME).push().setValue(chat); // 데이터 푸쉬
-                chat_edit.setText(""); //입력창 초기화
+                Calendar c = Calendar.getInstance();
+                SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH시mm분ss초");
+                String chatdate = df.format(c.getTime());
 
+                ChatDTO chat = new ChatDTO(USER_NAME, chat_edit.getText().toString()); //ChatDTO를 이용하여 데이터를 묶는다.
+                databaseReference.child("chat").child(CHAT_NAME).child(chatdate).setValue(chat); // 데이터 푸쉬
+                chat_edit.setText(""); //입력창 초기화
             }
         });
+
     }
     private void addMessage(DataSnapshot dataSnapshot, ArrayAdapter<String> adapter) {
         ChatDTO chatDTO = dataSnapshot.getValue(ChatDTO.class);
         adapter.add(chatDTO.getUserName() + " : " + chatDTO.getMessage());
     }
 
-    private void removeMessage(DataSnapshot dataSnapshot, ArrayAdapter<String> adapter) {
+    public void removeMessage(DataSnapshot dataSnapshot, ArrayAdapter<String> adapter) {
         ChatDTO chatDTO = dataSnapshot.getValue(ChatDTO.class);
         adapter.remove(chatDTO.getUserName() + " : " + chatDTO.getMessage());
     }
@@ -76,7 +94,41 @@ public class ChatActivity extends AppCompatActivity {
 
                 = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1);
         chat_view.setAdapter(adapter);
+        chat_view.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, final int position, long arg3) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(ChatActivity.this);
+
+
+                builder.setMessage("메세지를 삭제하시겠습니까?")
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setTitle("알림")
+                        .setCancelable(false)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+//                        테스트실패        removeMessage(dataSnapshot, adapter);
+//                                adapter.notifyDataSetChanged();//새로고침
+                                dialog.cancel();
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id){
+                                Toast.makeText(getApplicationContext(), "ID value is " +
+                                        Integer.toString(id), Toast.LENGTH_SHORT).show();
+                                dialog.cancel();
+                            }
+                        });
+
+                AlertDialog alert = builder.create();
+                alert.show();
+
+
+//                Toast.makeText(getApplicationContext(),
+//                        "selected : " + adapter.getItem(position)+mAuth.getCurrentUser().getEmail(),
+//                        Toast.LENGTH_SHORT).show();
+            }
+        });
         // 데이터 받아오기 및 어댑터 데이터 추가 및 삭제 등..리스너 관리
         databaseReference.child("chat").child(chatName).addChildEventListener(new ChildEventListener() {
             @Override
--- Android/memoproject/app/src/main/java/com/study/android/memoproject/login.java
+++ Android/memoproject/app/src/main/java/com/study/android/memoproject/login.java
@@ -17,6 +17,7 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.android.gms.common.SignInButton;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
